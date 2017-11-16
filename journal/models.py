4d6ef7dd374ce88d9bd5b6baa4da6a5028c197e3
George <george.christie@hotmail.co.uk>
2017-11-16 20:24:16 +0000

    Added uuid to static image urls

--- journal/models.py
+++ journal/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 from django.utils import timezone
+import uuid
+import os.path
 
 class JournalEntry(models.Model):
-    author = models.ForeignKey('main.CustomUser')
 
+    def get_filename(instance, filename):
+        return 'user_{0}/{1}{2}'.format(instance.author.id, uuid.uuid4(), filename)
+
+    author = models.ForeignKey('main.CustomUser')
     content = models.TextField(null=False, blank=False)
-    image = models.ImageField(upload_to='img/journalentries', null=True)
+    image = models.ImageField(upload_to=get_filename, null=True)
     created_date = models.DateTimeField(
         default=timezone.now)
     actual_date = models.DateField(unique=True, null=False, blank=False)
@@ -14,3 +19,6 @@ class JournalEntry(models.Model):
     word_of_the_day = models.CharField(max_length=30, null=True)
     word_of_the_day_def = models.TextField(max_length=200, null=True)
     posture_changes = models.PositiveIntegerField(null=True)
+
+
+# img/journalentries
